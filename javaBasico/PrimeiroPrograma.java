0e84c358d44d6d9b0ea0ca8541c4ea3073f2cf47
Matheus Filipe <math.filipe23@gmail.com>
2024-10-03 19:47:09 -0300

    Create PrimeiroPrograma.java

---
+++ javaBasico/PrimeiroPrograma.java
@@ -0,0 +1,7 @@
+package primeiroprograma; // PACOTE
+public class PrimeiroPrograma { // CLASSE
+    public static void main(String[] args){ // MÉTODO - "psvm" o atalho 
+        System.out.println("Olá, Mundo!"); // "sout" o atalho
+    }
+
+}
