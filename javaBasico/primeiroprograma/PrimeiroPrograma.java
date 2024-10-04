9250333f890fa16c155c695c06b4cee17ca036c9
Matheus Filipe <math.filipe23@gmail.com>
2024-10-03 21:01:30 -0300

    [fix] change-helloWorld

--- javaBasico/primeiroprograma/PrimeiroPrograma.java
+++ javaBasico/primeiroprograma/PrimeiroPrograma.java
@@ -1,4 +1,4 @@
-package primeiroprograma; // PACOTE
+package javaBasico.primeiroprograma; // PACOTE
 public class PrimeiroPrograma { // CLASSE
     public static void main(String[] args){ // MÉTODO - "psvm" o atalho 
         System.out.println("Olá, Mundo!"); // "sout" o atalho
