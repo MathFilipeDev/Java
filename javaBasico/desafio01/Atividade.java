a20beab53262df7179142269c19736522beb7f7b
Matheus Filipe <math.filipe23@gmail.com>
2024-11-15 18:32:55 -0300

    Create Atividade.java

---
+++ javaBasico/desafio01/Atividade.java
@@ -0,0 +1,12 @@
+package javaBasico.desafio01;
+
+public class Atividade {
+    public static void main(String[] args) {
+        String saudacao = "Olá, meu nome é ";
+        String nome = "Matheus";
+        String continuacao = " e minha idade é: ";
+        int idade = 24;
+        System.out.println(saudacao + nome + continuacao + idade);
+
+    }
+}
