8bc6b3a6cf3ae61e19581c9aac9891ba694e3547
Matheus Filipe <math.filipe23@gmail.com>
2024-11-17 17:56:56 -0300

    Update Maior.java

--- javaBasico/exercicios/Maior.java
+++ javaBasico/exercicios/Maior.java
@@ -5,17 +5,18 @@ import java.util.Scanner;
 public class Maior {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite um número: ");
+        System.out.println("Digite o primeiro número: ");
         int num1 = sc.nextInt();
+        System.out.println("Digite o segundo número: ");
         int num2 = sc.nextInt();
-        if (num1 == num2);
+
+        if (num1 == num2) {
             System.out.println("Os números são iguais!");
-        else if (num1 != num2);
-        
-
-
-
-
+        } else if (num1 > num2) {
+            System.out.println("Os números são diferentes e o primeiro número é maior");
+        } else {
+            System.out.println("Os números são diferentes e o segundo número é maior");
+        }
         sc.close();
     }
 }
