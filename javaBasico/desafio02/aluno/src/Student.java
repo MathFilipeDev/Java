4adc7bb182faec73b4f0a4266bddbcc7770bc69a
Matheus Filipe <math.filipe23@gmail.com>
2024-12-22 13:33:49 -0300

    Create Aluno

---
+++ javaBasico/desafio02/aluno/src/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    public static void main(String[] args) {
+        Aluno meuAluno = new Aluno();
+        meuAluno.nome = "Thor";
+        meuAluno.idade = 17;
+
+        meuAluno.exibeInformacoes();
+    }
+}
